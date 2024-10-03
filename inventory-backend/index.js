56026c9e2d618b062ab1cb7df6b1f9d883a03aa6
Emily Proctor <emilyproctorr@gmail.com>
2024-10-02 20:57:18 -0500

    initialize backend server

---
+++ inventory-backend/index.js
@@ -0,0 +1,19 @@
+// purpose of index.js -> establish logice for Node.js server, enable listening for incoming requests and respond to those requests
+
+// import
+const express = require('express');
+const cors = require('cors');
+
+// express instance
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express.js Server!</h1>');
+});
+
+// define port
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
